dffb8aedac658128604ccfef21c563a38443536a
ashirwad1902 <100486063+ashirwad1902@users.noreply.github.com>
2024-09-15 17:58:06 +0000

    file 1

---
+++ Basics/01_var_let_const.js
@@ -0,0 +1,12 @@
+const account_Id = 144553
+let account_Email="hello@gmail.com"
+var account_Password="12344"
+account_City="Indore"
+
+//account_Id = 2
+account_Email="heloo@hello.com"
+account_Password="1234"
+account_City="Bhopal"
+
+
+console.table([account_Id,account_Email,account_Password, account_City])
