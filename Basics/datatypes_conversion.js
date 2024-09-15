6f0f02e852f7865a6549001dc167fb55552dd41d
ashirwad1902 <100486063+ashirwad1902@users.noreply.github.com>
2024-09-15 20:59:06 +0000

    datataypes

---
+++ Basics/datatypes_conversion.js
@@ -0,0 +1,47 @@
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+let str1="hello"
+let str2 = " Ash"
+
+let str3=str1+str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+// console.log("2">1);
+
+// console.log("2">false);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+
+const id=Symbol('123')
+const anotherID=Symbol('123')
+
+console.log(id===anotherID);
+
+const name=new String('ashirwad')
+
+const anotherString= name.slice(2,4);
+console.log(anotherString);
+
+const notherString= name.slice(-8,3);
+console.log(notherString);
+
+const newString1 ="    ash    "
+console.log(newString1);
+console.log(newString1.trim());
+
+
